c6be5e10568d4cb6705ff9901561ab00ee264fd0
runarer <95697611+runarer@users.noreply.github.com>
2026-04-01 09:17:19 +0200

    Endpoint for signup

--- Source/NotesWeb/Features/Users/SignUp/Endpoint.cs
+++ Source/NotesWeb/Features/Users/SignUp/Endpoint.cs
@@ -2,16 +2,32 @@ using NotesWeb.Entities;
 
 namespace NotesWeb.Features.Users.SignUp;
 
-public class Endpoint : Endpoint<Request, Response, Mapper>
+public class Endpoint(TimeProvider timeProvider) : Endpoint<Request, Response, Mapper>
 {
+
+    private readonly TimeProvider _timeProvider = timeProvider;
+
     public override void Configure()
     {
-        Post("/author/signup");
+        Post("/users");
         AllowAnonymous();
     }
 
     public override async Task HandleAsync(Request request, CancellationToken ct)
     {
         User user = Map.ToEntity(request);
+        user.CreatedAtUtc = _timeProvider.GetUtcNow();
+        user.UpdatedAtUtc = user.CreatedAtUtc;
+
+        // TODO: Check if email is already used
+        // TODO: Check if username is taken
+
+        // Hash password
+
+        // TODO: write user to DB
+
+        //TODO CreatedAtAsync<Endpoint where user is available>()
+        var response = Map.FromEntity(user);
+        await Send.CreatedAtAsync("/users/", new { user.Id }, response, cancellation: ct);
     }
 }
