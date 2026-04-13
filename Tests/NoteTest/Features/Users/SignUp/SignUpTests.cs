df82c8d120457a2450a281b967933a3e82619cb2
runarer <95697611+runarer@users.noreply.github.com>
2026-04-13 09:39:03 +0200

    Adds test for signup

--- Tests/NoteTest/Features/Users/SignUp/SignUpTests.cs
+++ Tests/NoteTest/Features/Users/SignUp/SignUpTests.cs
@@ -1,48 +1,130 @@
 
 using System.Net;
-using NotesWeb.Entities;
 using NotesWeb.Features.Users.SignUp;
 
 namespace NoteTest.Features.Users.SignUp;
 
 public class SignUpTests(App App) : TestBase<App>
 {
-    [Fact]
+    private Request _validUser = new()
+    {
+        Username = "Bobb",
+        FullName = "Bob Bobby Bobson",
+        Email = "bob@example.com",
+        Password = "BobIsAwesome"
+    };
+
+
+    [Fact, Priority(1)]
     public async Task ValidUserSignup_ReturnsOkAndUserObject()
     {
-        var validUser = new Request()
-        {
-            Username = "Bobb",
-            FullName = "Bob Bobby Bobson",
-            Email = "bob@example.com",
-            Password = "BobIsAwesome"
-        };
-
-        var (rsp, res) = await App.Client.POSTAsync<SignUpEndpoint, Request, Response>(validUser);
+        var (rsp, res) = await App.Client.POSTAsync<SignUpEndpoint, Request, Response>(_validUser);
 
         Assert.Equal(HttpStatusCode.Created, rsp.StatusCode);
         Assert.NotNull(res);
     }
 
-    [Fact]
-    public async Task InvalidSignup_ReturnsBadRequestAndProblemDetails()
+    [Fact, Priority(2)]
+    public async Task UsernameTaken_ReturnsOkAndUserObject()
     {
         var expected = new[] {
-            ("username", "Username to short"),
-            ("email", "Email providet not valid"),
-            ("password", "Password to short") };
+            ("username", "this username is taken!")};
+        Request newUser = _validUser;
+        newUser.Email = "F" + _validUser.Email;
 
-        var invalidUser = new Request()
-        {
-            Username = "B",
-            Email = "fffakkke",
-            Password = "tw"
-        };
-
-        var (rsp, res) = await App.Client.POSTAsync<SignUpEndpoint, Request, FastEndpoints.ProblemDetails>(invalidUser);
+        var (rsp, res) = await App.Client.POSTAsync<SignUpEndpoint, Request, ProblemDetails>(newUser);
 
         Assert.Equal(HttpStatusCode.BadRequest, rsp.StatusCode);
-        Assert.Equal(3, res.Errors.Count());
+        Assert.Single(res.Errors);
+        Assert.Equivalent(expected, res.Errors.Select(e => (e.Name, e.Reason)));
+    }
+    [Fact, Priority(2)]
+    public async Task EmailTaken_ReturnsOkAndUserObject()
+    {
+        var expected = new[] {
+            ("email", "this email is already used!")};
+        Request newUser = _validUser;
+        newUser.Username = "F" + _validUser.Username;
+
+        var (rsp, res) = await App.Client.POSTAsync<SignUpEndpoint, Request, ProblemDetails>(newUser);
+
+        Assert.Equal(HttpStatusCode.BadRequest, rsp.StatusCode);
+        Assert.Single(res.Errors);
+        Assert.Equivalent(expected, res.Errors.Select(e => (e.Name, e.Reason)));
+    }
+
+    [Fact]
+    public async Task InvalidEmail_ReturnsBadRequestAndProblemDetails()
+    {
+        var expected = new[] {
+            ("email", "Email providet is not a valid email")};
+
+        var invalidUser = _validUser;
+        invalidUser.Email = "fffakkke";
+
+        var (rsp, res) = await App.Client.POSTAsync<SignUpEndpoint, Request, ProblemDetails>(invalidUser);
+
+        Assert.Equal(HttpStatusCode.BadRequest, rsp.StatusCode);
+        Assert.Single(res.Errors);
+        Assert.Equivalent(expected, res.Errors.Select(e => (e.Name, e.Reason)));
+    }
+
+    [Fact]
+    public async Task UsernameToLong_ReturnsBadRequestAndProblemDetails()
+    {
+        var invalidUser = _validUser;
+        invalidUser.Username = "Boobbbbbbbbbbbbbbbyyyyyyyyyyyyyyyyyyyyy";
+        var expected = new[] {
+            ("username", "Username is to long") };
+
+        var (rsp, res) = await App.Client.POSTAsync<SignUpEndpoint, Request, ProblemDetails>(invalidUser);
+
+        Assert.Equal(HttpStatusCode.BadRequest, rsp.StatusCode);
+        Assert.Single(res.Errors);
+        Assert.Equivalent(expected, res.Errors.Select(e => (e.Name, e.Reason)));
+    }
+
+    [Fact]
+    public async Task PasswordToLong_ReturnsBadRequestAndProblemDetails()
+    {
+        var invalidUser = _validUser;
+        invalidUser.Password = "Boobbbbbbbbbbbbbbbyyyyyyyyyyyyyyyyyyyyy";
+        var expected = new[] {
+            ("password", "Password is to long") };
+
+        var (rsp, res) = await App.Client.POSTAsync<SignUpEndpoint, Request, ProblemDetails>(invalidUser);
+
+        Assert.Equal(HttpStatusCode.BadRequest, rsp.StatusCode);
+        Assert.Single(res.Errors);
+        Assert.Equivalent(expected, res.Errors.Select(e => (e.Name, e.Reason)));
+    }
+    [Fact]
+    public async Task UsernameToShort_ReturnsBadRequestAndProblemDetails()
+    {
+        var invalidUser = _validUser;
+        invalidUser.Username = "B";
+        var expected = new[] {
+            ("username", "Username is to short") };
+
+        var (rsp, res) = await App.Client.POSTAsync<SignUpEndpoint, Request, ProblemDetails>(invalidUser);
+
+        Assert.Equal(HttpStatusCode.BadRequest, rsp.StatusCode);
+        Assert.Single(res.Errors);
+        Assert.Equivalent(expected, res.Errors.Select(e => (e.Name, e.Reason)));
+    }
+
+    [Fact]
+    public async Task PasswordToShort_ReturnsBadRequestAndProblemDetails()
+    {
+        var invalidUser = _validUser;
+        invalidUser.Password = "Bo";
+        var expected = new[] {
+            ("password", "Password is to short") };
+
+        var (rsp, res) = await App.Client.POSTAsync<SignUpEndpoint, Request, ProblemDetails>(invalidUser);
+
+        Assert.Equal(HttpStatusCode.BadRequest, rsp.StatusCode);
+        Assert.Single(res.Errors);
         Assert.Equivalent(expected, res.Errors.Select(e => (e.Name, e.Reason)));
     }
 }
