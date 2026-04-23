0916810453bc381b639a62a48d301b1cb1237186
runarer <95697611+runarer@users.noreply.github.com>
2026-04-23 12:49:52 +0200

    Implements RenameToDoItemendpoint

---
+++ Source/NotesWeb/Features/ToDo/ToDoItems/RenameToDoItem/Model.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace NotesWeb.Features.ToDo.ToDoItems.RenameToDoItem;
+
+public class Request
+{
+    [FromClaim] public int UserId { get; set; }
+    public Guid ItemId { get; set; }
+    public Guid ListId { get; set; }
+    public required string Title { get; set; }
+
+}
+
+public class Response
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public bool Completed { get; set; }
+    public Guid ParentListId { get; set; }
+    public DateTimeOffset CreatedAtUtc { get; set; }
+    public DateTimeOffset UpdatedAtUtc { get; set; }
+}
+
+public class Validator : Validator<Request>
+{
+    public Validator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("You need to provide a title")
+            .MinimumLength(3).WithMessage("Title is to short")
+            .MaximumLength(30).WithMessage("Title is to long");
+    }
+}
