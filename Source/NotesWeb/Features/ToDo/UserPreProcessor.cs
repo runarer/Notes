c080b9a830922836e02e7564ec6a8066a833684e
runarer <95697611+runarer@users.noreply.github.com>
2026-04-29 13:50:19 +0200

    Independent from request DTOs

--- Source/NotesWeb/Features/ToDo/UserPreProcessor.cs
+++ Source/NotesWeb/Features/ToDo/UserPreProcessor.cs
@@ -1,4 +1,5 @@
 
+using FastEndpoints.Security;
 using Microsoft.EntityFrameworkCore;
 using NotesWeb.Data;
 
@@ -12,27 +13,27 @@ public class UserPreProcessor/*(NoteBoardDBContext dbContext)*/ : IPreProcessor<
 {
     //     private readonly NoteBoardDBContext _dbContext = dbContext;
 
-    public async Task PreProcessAsync(IPreProcessorContext<UserRequest> context, CancellationToken ct)
-    {
-        var dbContext = context.HttpContext.RequestServices.GetRequiredService<NoteBoardDBContext>();
-        //Check if User exists
-        if (context.Request is null || !await dbContext.Users.AnyAsync(user => user.Id == context.Request.UserId, ct))
-            await context.HttpContext.Response.SendUnauthorizedAsync(ct);
-
-    }
-
-    //     public async Task PreProcessAsync(IPreProcessorContext<UserRequest> context, CancellationToken ct)
+    // public async Task PreProcessAsync(IPreProcessorContext<UserRequest> context, CancellationToken ct)
     // {
-    //     // Get user id from claims, this makes the preprocessor independent
-    //     var userIdClaim = context.HttpContext.User.ClaimValue("UserId");
-    //     int userId = 0;
-    //     if (userIdClaim is null || !int.TryParse(userIdClaim, out userId))
-    //         await context.HttpContext.Response.SendUnauthorizedAsync(ct);
-
     //     var dbContext = context.HttpContext.RequestServices.GetRequiredService<NoteBoardDBContext>();
     //     //Check if User exists
-    //     if (!await dbContext.Users.AnyAsync(user => user.Id == userId, ct))
+    //     if (context.Request is null || !await dbContext.Users.AnyAsync(user => user.Id == context.Request.UserId, ct))
     //         await context.HttpContext.Response.SendUnauthorizedAsync(ct);
 
     // }
+
+    public async Task PreProcessAsync(IPreProcessorContext<UserRequest> context, CancellationToken ct)
+    {
+        // Get user id from claims, this makes the preprocessor independent
+        var userIdClaim = context.HttpContext.User.ClaimValue("UserId");
+        int userId = 0;
+        if (userIdClaim is null || !int.TryParse(userIdClaim, out userId))
+            await context.HttpContext.Response.SendUnauthorizedAsync(ct);
+
+        var dbContext = context.HttpContext.RequestServices.GetRequiredService<NoteBoardDBContext>();
+        //Check if User exists
+        if (!await dbContext.Users.AnyAsync(user => user.Id == userId, ct))
+            await context.HttpContext.Response.SendUnauthorizedAsync(ct);
+
+    }
 }
