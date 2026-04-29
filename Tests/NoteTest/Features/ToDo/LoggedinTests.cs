eb064140f1384606ca3169aca261f8b34cbd88de
runarer <95697611+runarer@users.noreply.github.com>
2026-04-29 09:12:19 +0200

    [Green] Renamelisttest passes

--- Tests/NoteTest/Features/ToDo/LoggedinTests.cs
+++ Tests/NoteTest/Features/ToDo/LoggedinTests.cs
@@ -2,9 +2,11 @@ using System.Net;
 
 namespace NoteTest.Features.ToDo;
 
-public sealed class LoginState : StateFixture
+public class LoginState : StateFixture
 {
     public bool Token = false;
+    public Guid ListId = default;
+    public Guid ItemId = default;
 }
 
 public class LoggedinTests(App app, LoginState state) : TestBase<App, LoginState>//, IAsyncLifetime
@@ -51,5 +53,28 @@ public class LoggedinTests(App app, LoginState state) : TestBase<App, LoginState
 
             State.Token = true;
         }
+    }
+
+    protected async Task SetList()
+    {
+        await SetTokenAsync();
+        State.ListId = await CreateAListAsync("Testing list");
+    }
+
+    protected async Task<Guid> CreateAListAsync(string title)
+    {
+        var (rsp, res) = await App.Client.POSTAsync<
+            NotesWeb.Features.ToDo.ToDoLists.CreateList.CreateListEndpoint,
+            NotesWeb.Features.ToDo.ToDoLists.CreateList.Request,
+            NotesWeb.Features.ToDo.ToDoLists.CreateList.Response>(
+                new NotesWeb.Features.ToDo.ToDoLists.CreateList.Request
+                {
+                    Title = title
+                });
+        Assert.Equal(HttpStatusCode.Created, rsp.StatusCode);
+        Assert.NotNull(res);
+        Assert.NotEqual(default, res.Id);
+
+        return res.Id;
     }
 }
