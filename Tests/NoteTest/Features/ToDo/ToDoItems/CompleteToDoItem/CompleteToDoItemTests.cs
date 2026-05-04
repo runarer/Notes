1df346639e188187dba5f565928a90cea4171ad5
runarer <95697611+runarer@users.noreply.github.com>
2026-05-04 11:28:01 +0200

    [Green] Complete todo item passes

--- Tests/NoteTest/Features/ToDo/ToDoItems/CompleteToDoItem/CompleteToDoItemTests.cs
+++ Tests/NoteTest/Features/ToDo/ToDoItems/CompleteToDoItem/CompleteToDoItemTests.cs
@@ -1,42 +1,148 @@
-using System;
+
+using System.Net;
+using NotesWeb.Features.ToDo.ToDoItems.CompleteToDoItem;
 
 namespace NoteTest.Features.ToDo.ToDoItems.CompleteToDoItem;
 
-public class CompleteToDoItemTests
+public class CompleteToDoItemTests(App App, LoginState State) : LoggedinTests(App, State)
 {
 
-    // Todo: add a getitem endpoint
-    // Todo: add a getitem endpoint test
+    [Fact]
+    public async Task CompleteToDoItem_ItemIsCreatedAndCompleted_ItemCompleted()
+    {
+        // SignUp user
+        await SetTokenAsync();
 
-    // public async Task CompleteToDoItem_ItemIsCreatedAndCompleted_ItemCompleted()
-    // Create a list
-    // Add an item
-    // Complete item
-    // Get Item 
-    // Assert it's completed
-    // Assert updated time is correct
+        // Create a list
+        var listId = await CreateAListAsync("List for testing complet item");
+        // Add an item
+        string item = "Test item 1";
+        var itemId = await CreateAnItemAsync(listId, item);
+        //Advance time so we can test updatetime
+        App.FakeTime.Advance(TimeSpan.FromHours(2));
+        var fakeTime = App.FakeTime.GetUtcNow();
+        // Complete item
+        var rspComplete = await App.Client.PATCHAsync<CompleteToDoItemEndpoint, Request>(new Request
+        {
+            ListId = listId,
+            ItemId = itemId
+        });
 
 
-    // public async Task CompleteToDoItem_ItemIsCreatedAndCompletedThenUncompleted_ItemIsUncompleted()
-    // Create a list
-    // Add an item
-    // Complete item -> other tests assert item is set to completed
-    // Uncomplete item
-    // Assert item is uncompleted
-    // Assert item update time is correct
+        Assert.Equal(HttpStatusCode.OK, rspComplete.StatusCode);
+        // Get Item 
+        var (rsp, res) = await App.Client.GETAsync<
+        NotesWeb.Features.ToDo.ToDoItems.GetItem.GetItemEndpoint,
+        NotesWeb.Features.ToDo.ToDoItems.GetItem.Request,
+        NotesWeb.Features.ToDo.ToDoItems.GetItem.Response>(
+            new NotesWeb.Features.ToDo.ToDoItems.GetItem.Request
+            {
+                ListId = listId,
+                ItemId = itemId
+            });
+        Assert.Equal(HttpStatusCode.OK, rsp.StatusCode);
+        Assert.NotNull(res);
+        // Assert it's completed
+        Assert.True(res.Completed);
+        // Assert updated time is correct
+        Assert.NotEqual(res.CreatedAtUtc, res.UpdatedAtUtc);
+        Assert.Equal(fakeTime, res.UpdatedAtUtc);
 
-    // public async Task CompleteToDoItem_ItemIsCreatedAndCompleted with query_ItemCompleted()
-    // Create a list
-    // Add an item
-    // Complete item with query
-    // Assert item compleded
 
-    //public async Task CompleteToDoItem_ItemDoNotExist_ReturnsNotFound()
-    // Create a list
-    // Complete a non-existing item in the list
-    // Assert 404
+    }
 
-    // public async Task CompleteToDoItem_ListDoesNotExist_ReturnsNotFound()
-    // Complete an item in a non-existing list
-    // Assert 404
+    [Fact]
+    public async Task CompleteToDoItem_ItemIsCreatedAndCompletedThenUncompleted_ItemIsUncompleted()
+    {
+        // SignUp user
+        await SetTokenAsync();
+
+        // Create a list
+        var listId = await CreateAListAsync("List for testing uncomplete");
+        // Add an item
+        string item = "Test item 1";
+        var itemId = await CreateAnItemAsync(listId, item);
+
+        // Complete item -> other tests assert item is set to completed
+        var rspComplete = await App.Client.PATCHAsync<CompleteToDoItemEndpoint, Request>(new Request
+        {
+            ListId = listId,
+            ItemId = itemId
+        });
+        Assert.Equal(HttpStatusCode.OK, rspComplete.StatusCode);
+
+        //Advance time so we can test updatetime
+        App.FakeTime.Advance(TimeSpan.FromHours(2));
+        var fakeTime = App.FakeTime.GetUtcNow();
+
+
+
+        var rspUncomplete = await App.Client.PATCHAsync<CompleteToDoItemEndpoint, Request>(new Request
+        {
+            ListId = listId,
+            ItemId = itemId,
+            Query = new() { Completed = false }
+        });
+
+
+
+
+        Assert.Equal(HttpStatusCode.OK, rspUncomplete.StatusCode);
+        // Get Item 
+        var (rsp, res) = await App.Client.GETAsync<
+        NotesWeb.Features.ToDo.ToDoItems.GetItem.GetItemEndpoint,
+        NotesWeb.Features.ToDo.ToDoItems.GetItem.Request,
+        NotesWeb.Features.ToDo.ToDoItems.GetItem.Response>(
+            new NotesWeb.Features.ToDo.ToDoItems.GetItem.Request
+            {
+                ListId = listId,
+                ItemId = itemId
+            });
+        Assert.Equal(HttpStatusCode.OK, rsp.StatusCode);
+        Assert.NotNull(res);
+        // Assert it's not completed
+        Assert.False(res.Completed);
+        // Assert updated time is correct
+        Assert.NotEqual(res.CreatedAtUtc, res.UpdatedAtUtc);
+        Assert.Equal(fakeTime, res.UpdatedAtUtc);
+    }
+
+    [Fact]
+    public async Task CompleteToDoItem_ItemDoNotExist_ReturnsNotFound()
+    {
+        await SetTokenAsync();
+
+        // Create a list
+        var listId = await CreateAListAsync("List for testing uncomplete");
+
+
+        // Complete a non-existing item in the list
+        var rsp = await App.Client.PATCHAsync<CompleteToDoItemEndpoint, Request>(new Request
+        {
+            ListId = listId,
+            ItemId = Guid.NewGuid()
+        });
+
+
+        // Assert 404
+        Assert.Equal(HttpStatusCode.NotFound, rsp.StatusCode);
+    }
+
+    [Fact]
+    public async Task CompleteToDoItem_ListDoesNotExist_ReturnsNotFound()
+    {
+        await SetTokenAsync();
+
+
+        // Complete an item in a non-existing list
+        var rsp = await App.Client.PATCHAsync<CompleteToDoItemEndpoint, Request>(new Request
+        {
+            ListId = Guid.NewGuid(),
+            ItemId = Guid.NewGuid()
+        });
+
+
+        // Assert 404
+        Assert.Equal(HttpStatusCode.NotFound, rsp.StatusCode);
+    }
 }
