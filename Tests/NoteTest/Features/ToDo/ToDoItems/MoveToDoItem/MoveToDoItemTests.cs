bf9dec8323c597cbbf801d0f00a54f332df8a598
runarer <95697611+runarer@users.noreply.github.com>
2026-05-04 14:21:23 +0200

    [Green] Missing tests added and passes

--- Tests/NoteTest/Features/ToDo/ToDoItems/MoveToDoItem/MoveToDoItemTests.cs
+++ Tests/NoteTest/Features/ToDo/ToDoItems/MoveToDoItem/MoveToDoItemTests.cs
@@ -33,29 +33,50 @@ public class MoveToDoItemTests(App App, LoginState State) : LoggedinTests(App, S
         Assert.Equal(secondList, res.ParentListId);
     }
     [Fact]
-    public async Task MoveToDoItem_CreateOneListAddItemMoveItemToNonExistingList_ReturnProblemDetails()
+    public async Task MoveToDoItem_CreateOneListAddItemMoveItemToNonExistingList_ReturnNotFound()
     {
         await SetTokenAsync();
         // Create first list
+        var firstList = await CreateAListAsync("First List");
+        // Create second list
+        var secondList = Guid.NewGuid();
         // Create item in first list
-        // Move item form first list to non-existing list
-        // Assert problem details
-    }
+        var itemId = await CreateAnItemAsync(firstList, "Item to move");
 
-    [Fact]
-    public async Task MoveToDoItem_MoveAnItemFromAnNonExistingList_ReturnNotFound()
-    {
-        await SetTokenAsync();
-        // Move Item from list to list
-        // Assert NotFound
+        var request = new Request
+        {
+            ItemId = itemId,
+            ToList = secondList
+
+        };
+
+        // Move item from first to second list
+        var (rsp, _) = await App.Client.PATCHAsync<MoveToDoItemEndpoint, Request, Response>(request);
+
+        // Assert item is in second list
+        Assert.Equal(HttpStatusCode.NotFound, rsp.StatusCode);
     }
 
     [Fact]
     public async Task MoveToDoItem_CreateAListMoveAnNonExistingItemFromItToAList_ReturnsNotFound()
     {
         await SetTokenAsync();
-        // Create list
-        // Move Item
-        // Assert NotFound
+        // Create target list
+        var secondList = Guid.NewGuid();
+        // Create item in first list
+        var itemId = Guid.NewGuid();
+
+        var request = new Request
+        {
+            ItemId = itemId,
+            ToList = secondList
+
+        };
+
+        // Move item from first to second list
+        var (rsp, _) = await App.Client.PATCHAsync<MoveToDoItemEndpoint, Request, Response>(request);
+
+        // Assert item is in second list
+        Assert.Equal(HttpStatusCode.NotFound, rsp.StatusCode);
     }
 }
