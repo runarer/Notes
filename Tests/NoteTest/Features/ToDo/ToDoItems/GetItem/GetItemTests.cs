953506a611c5653b781df49988bcc89b1a8142ef
runarer <95697611+runarer@users.noreply.github.com>
2026-05-04 09:36:24 +0200

    [Green] Tests for GetItem created and passes

--- Tests/NoteTest/Features/ToDo/ToDoItems/GetItem/GetItemTests.cs
+++ Tests/NoteTest/Features/ToDo/ToDoItems/GetItem/GetItemTests.cs
@@ -1,21 +1,91 @@
 
+using System.Net;
+using NotesWeb.Features.ToDo.ToDoItems.GetItem;
+
 namespace NoteTest.Features.ToDo.ToDoItems.GetItem;
 
-public class GetItemTests
+public class GetItemTests(App App, LoginState State) : LoggedinTests(App, State)
 {
-    // public async Task GetItem_CreateAnItemAndGetItBack_ItemIsReturned()
-    // Create a list
-    // Add an item
-    // Get Item back
-    // Assert same title
+    private readonly NotesWeb.Features.ToDo.ToDoItems.CreateToDoItem.Request _validRequest = new()
+    {
+        Title = "A testing list"
+    };
+    [Fact]
+    public async Task GetItem_CreateAnItemAndGetItBack_ItemIsReturned()
+    {
+        // SignUp user
+        await SetTokenAsync();
+        // Create a list
+        var listId = await CreateAListAsync("Test list for adding items");
+        _validRequest.ListId = listId;
+        // Add an item
+        var (rspPost, resPost) = await App.Client.POSTAsync<
+            NotesWeb.Features.ToDo.ToDoItems.CreateToDoItem.CreateToDoItemEndpoint,
+            NotesWeb.Features.ToDo.ToDoItems.CreateToDoItem.Request,
+            NotesWeb.Features.ToDo.ToDoItems.CreateToDoItem.Response>(_validRequest);
+        Assert.Equal(HttpStatusCode.Created, rspPost.StatusCode);
+        Assert.NotNull(resPost);
 
-    // public async Task GetItem_ItemDoesNotExist_ReturnsNotFound()
-    // Create a list
-    // Get Item from list
-    // Assert response NotFound
 
-    // public async Task GetItem_ListDoesNotExists_ReturnsNotFound()
-    // Get Item in list
-    // Assert respone NotFound
+        // Get Item back
+        var (rsp, res) = await App.Client.GETAsync<GetItemEndpoint, Request, Response>(
+            new Request
+            {
+                ListId = listId,
+                ItemId = resPost.Id
+            });
+
+
+        Assert.Equal(HttpStatusCode.OK, rsp.StatusCode);
+        Assert.NotNull(res);
+
+        // Assert same title
+        Assert.Equal(_validRequest.Title, res.Title);
+    }
+
+    [Fact]
+    public async Task GetItem_ItemDoesNotExist_ReturnsNotFound()
+    {
+        // SignUp user
+        await SetTokenAsync();
+        // Create a list
+        var listId = await CreateAListAsync("Test list for adding items");
+        _validRequest.ListId = listId;
+
+
+        // Get Item from list
+        var (rsp, res) = await App.Client.GETAsync<GetItemEndpoint, Request, Response>(
+            new Request
+            {
+                ListId = listId,
+                ItemId = Guid.NewGuid()
+            });
+
+
+        // Assert response NotFound
+        Assert.Equal(HttpStatusCode.NotFound, rsp.StatusCode);
+        Assert.Null(res);
+
+    }
+    [Fact]
+    public async Task GetItem_ListDoesNotExists_ReturnsNotFound()
+    {
+        // SignUp user
+        await SetTokenAsync();
+
+
+        // Get Item in list
+        var (rsp, res) = await App.Client.GETAsync<GetItemEndpoint, Request, Response>(
+            new Request
+            {
+                ListId = Guid.NewGuid(),
+                ItemId = Guid.NewGuid()
+            });
+
+
+        // Assert response NotFound
+        Assert.Equal(HttpStatusCode.NotFound, rsp.StatusCode);
+        Assert.Null(res);
+    }
 
 }
